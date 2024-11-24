0481397cf0f28c26e929f0ec63455b4d98921b1b
LucaDal <dalessandroluca1@gmail.com>
2024-11-24 19:47:27 +0100

    Semplificata la scrittura e lettura

--- AuthenticationManager.cpp
+++ AuthenticationManager.cpp
@@ -1,24 +1,20 @@
 #include "AuthenticationManager.h"
 
-AuthManger::AuthManger(size_t size_username, size_t size_password, size_t start_address, size_t eeprom_size){
-    this->max_size_username = size_username;
-    this->max_size_password = size_password;
+/// @brief username and password can be of 255 char max
+/// @param start_address 
+/// @param eeprom_size 
+AuthManger::AuthManger(size_t start_address, size_t eeprom_size){
     this->start_address = start_address;
     EEPROM.begin(eeprom_size);
 }
 
- void write(const char * value, size_t max_lenght, size_t start_address){
+void write(String value, size_t start_address){
     
-    for (size_t i = 0; i < max_lenght; i++){
-        if(i < strlen(value)){
-            EEPROM.write(start_address + i, value[i]);
-        }else{
-            EEPROM.write(start_address + i, 0);
-        }
-    }
+    for (size_t i = 0; i < value.length(); i++)
+        EEPROM.write(start_address + i, value[i]);
 }
 
- char * read(size_t size_to_read, size_t start_address){
+ char * read(size_t size_to_read, byte start_address){
     char * ptr = (char *) malloc(size_to_read * sizeof(char));
     for (size_t i = 0; i < size_to_read; i++){
         ptr[i] = (char)EEPROM.read(start_address + i);
@@ -27,24 +23,27 @@ AuthManger::AuthManger(size_t size_username, size_t size_password, size_t start_
 }
 
 extern char * AuthManger::getUserame(){
-    return read(this->max_size_username, this->start_address);
+    size_t user_length = (size_t)EEPROM.read(start_address);
+    return read(user_length, this->start_address + 2);
 }
 
 extern char * AuthManger::getPassword(){
-    return read(this->max_size_password, (this->start_address + this->max_size_username));
+    size_t user_length = (size_t)EEPROM.read(start_address);
+    size_t psw_length = (size_t)EEPROM.read(start_address + 1);
+    return read(psw_length, (this->start_address + user_length + 2));
 }
 
-extern void AuthManger::writeUsername(const char * username){
-    write(username, this->max_size_username, this->start_address);
-    EEPROM.commit();
-}
-extern void AuthManger::writePassword(const char * password){
-    write(password, this->max_size_password, (this->start_address + this->max_size_username));
-    EEPROM.commit();
-}
+extern void AuthManger::writeUsernameAndPassword(const char * username, const char * password){
 
-extern void AuthManger::writeUsernameAndPassword(const char * username, const char * psw){
-    write(username, this->max_size_username, this->start_address);
-    write(psw, this->max_size_password, this->start_address + this->max_size_username);
+    String tmp_user = String(username);
+    String tmp_psw = String(password);
+    //write at start address len of username
+    EEPROM.write(start_address, (byte)tmp_user.length());
+    //write at start address + 1 len of psw
+    EEPROM.write(start_address + 1, (byte)tmp_psw.length());
+    
+    write(tmp_user, (size_t)(this->start_address + 2));
+    write(tmp_psw, (size_t)(this->start_address + tmp_user.length() + 2));
+
     EEPROM.commit();
 }
--- AuthenticationManager.h
+++ AuthenticationManager.h
@@ -6,15 +6,11 @@
 //Scrivi su eeprom o leggi da eprom username and password;
 class AuthManger{
     private:
-        size_t max_size_username;
-        size_t max_size_password;
         size_t start_address;
     public:
-        AuthManger(size_t size_username, size_t size_password, size_t start_address, size_t eeprom_size);
+        AuthManger(size_t start_address, size_t eeprom_size);
         char *getUserame();
         char *getPassword();
-        void writePassword(const char * psw);
-        void writeUsername(const char * username);
         void writeUsernameAndPassword(const char * username, const char * password);
 };
 
