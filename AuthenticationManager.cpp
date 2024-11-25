8f6cb3c1362c34ce3500260ca184432d4eabc1c5
LucaDal <dalessandroluca1@gmail.com>
2024-11-25 20:56:55 +0100

    fixed written length

--- AuthenticationManager.cpp
+++ AuthenticationManager.cpp
@@ -8,42 +8,41 @@ AuthManger::AuthManger(size_t start_address, size_t eeprom_size){
     EEPROM.begin(eeprom_size);
 }
 
-void write(String value, size_t start_address){
+void write(const char * value, size_t address){
     
-    for (size_t i = 0; i < value.length(); i++)
-        EEPROM.write(start_address + i, value[i]);
+    for (size_t i = 0; i < strlen(value); i++)
+        EEPROM.write(address + i, value[i]);
 }
 
- char * read(size_t size_to_read, byte start_address){
+ char * read(size_t size_to_read, byte address){
     char * ptr = (char *) malloc(size_to_read * sizeof(char));
     for (size_t i = 0; i < size_to_read; i++){
-        ptr[i] = (char)EEPROM.read(start_address + i);
+        ptr[i] = (char)EEPROM.read(address + i);
     }
     return ptr;
 }
 
 extern char * AuthManger::getUserame(){
-    size_t user_length = (size_t)EEPROM.read(start_address);
+    size_t user_length = (size_t)EEPROM.read(this->start_address);
     return read(user_length, this->start_address + 2);
 }
 
 extern char * AuthManger::getPassword(){
-    size_t user_length = (size_t)EEPROM.read(start_address);
-    size_t psw_length = (size_t)EEPROM.read(start_address + 1);
+    size_t user_length = (size_t)EEPROM.read(this->start_address);
+    size_t psw_length = (size_t)EEPROM.read(this->start_address + 1);
     return read(psw_length, (this->start_address + user_length + 2));
 }
 
 extern void AuthManger::writeUsernameAndPassword(const char * username, const char * password){
 
-    String tmp_user = String(username);
-    String tmp_psw = String(password);
+
     //write at start address len of username
-    EEPROM.write(start_address, (byte)tmp_user.length());
+    EEPROM.write(this->start_address, (byte)strlen(username));
     //write at start address + 1 len of psw
-    EEPROM.write(start_address + 1, (byte)tmp_psw.length());
+    EEPROM.write(this->start_address + 1, (byte)strlen(password));
     
-    write(tmp_user, (size_t)(this->start_address + 2));
-    write(tmp_psw, (size_t)(this->start_address + tmp_user.length() + 2));
+    write(username, (size_t)(this->start_address + 2));
+    write(password, (size_t)(this->start_address + 2 + strlen(username)));
 
     EEPROM.commit();
 }
